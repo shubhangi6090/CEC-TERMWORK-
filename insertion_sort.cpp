eb5e17de9199c7207133d222e1f4e0b7a09400e1
Shubhangi Gupta <100912687+shubhangi6090@users.noreply.github.com>
2022-12-19 23:59:32 +0530

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,53 @@
+/*
+time complexity:
+worst case = O(n^2),
+best case = O(n)
+space complexity = O(1)
+*/
+
+#include<iostream>
+using namespace std;
+
+void insertion_sort(int *a,int n)
+{
+    int i,j;
+    int comps=0,shifts=0,key;
+
+    for(i=1;i<n;i++)
+    {
+        key=a[i];
+        j=i-1;
+
+        while (j>=0 && a[j]>key)
+        {
+            comps++;
+            shifts++;
+            a[j+1]=a[j];
+            j--;
+        }
+        shifts++;
+        a[j+1]=key;
+    }
+
+    for(i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
+
+    cout<<"comparisons = "<<comps<<endl;
+    cout<<"shifts = "<<shifts<<endl;
+}
+
+int main()
+{
+    i
+        int i,n;
+        cin>>n;
+
+        int a[n];
+        for(i=0;i<n;i++)
+        cin>>a[i];
+
+        insertion_sort(a,n);
+    
+    return 0;
+}
