5311b4377ba9aa2084261d8556a70cdb4a394617
Shubhangi Gupta <100912687+shubhangi6090@users.noreply.github.com>
2022-12-19 23:58:35 +0530

    Create linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,43 @@
+/*
+Time complexity =O(n)
+Space complexity=O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+void linear_search(int *a, int l, int num)
+{
+    int i, count = 0;
+    for (i = 0; i < l; i++)
+    {
+        count++;
+        if (a[i] == num)
+        {
+            cout << "Present " << count;
+            return;
+        }
+    }
+    cout << "Not Present " << count;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n, key, i;
+
+        cin >> n;
+
+        int a[n];
+        for (i = 0; i < n; i++)
+            cin >> a[i];
+
+        cin >> key;
+
+        linear_search(a, n, key);
+    }
+    return 0;
+}
